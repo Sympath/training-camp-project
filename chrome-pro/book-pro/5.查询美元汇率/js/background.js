6932f3e17bddeab65aa16f75c6a017bd1d322977
Wzyan <3101885298@qq.com>
2022-09-24 14:33:08 +0800

    训练营 ｜ chrome扩展实战 ｜5.查询美元汇率

--- chrome-pro/book-pro/5.查询美元汇率/js/background.js
+++ chrome-pro/book-pro/5.查询美元汇率/js/background.js
@@ -1,7 +1,7 @@
-function httpRequest(url, callback){
+function httpRequest(url, callback) {
     var xhr = new XMLHttpRequest();
     xhr.open("GET", url, true);
-    xhr.onreadystatechange = function() {
+    xhr.onreadystatechange = function () {
         if (xhr.readyState == 4) {
             callback(xhr.responseText);
         }
@@ -9,42 +9,42 @@ function httpRequest(url, callback){
     xhr.send();
 }
 
-function updateAmount(amount, exchange){
+function updateAmount(amount, exchange) {
     amount = Number(amount);
-    if(isNaN(amount) || !amount){
+    if (isNaN(amount) || !amount) {
         exchange([{
-            'content': '$1 = ¥'+price,
-            'description': '$1 = ¥'+price
-        },{
-            'content': '¥1 = $'+(1/price).toFixed(6),
-            'description': '¥1 = $'+(1/price).toFixed(6)
+            'content': '$1 = ¥' + price,
+            'description': '$1 = ¥' + price
+        }, {
+            'content': '¥1 = $' + (1 / price).toFixed(6),
+            'description': '¥1 = $' + (1 / price).toFixed(6)
         }]);
     }
-    else{
+    else {
         exchange([{
-            'content': '$'+amount+' = ¥'+(amount*price).toFixed(2),
-            'description': '$'+amount+' = ¥'+(amount*price).toFixed(2)
-        },{
-            'content': '¥'+amount+' = $'+(amount/price).toFixed(6),
-            'description': '¥'+amount+' = $'+(amount/price).toFixed(6)
+            'content': '$' + amount + ' = ¥' + (amount * price).toFixed(2),
+            'description': '$' + amount + ' = ¥' + (amount * price).toFixed(2)
+        }, {
+            'content': '¥' + amount + ' = $' + (amount / price).toFixed(6),
+            'description': '¥' + amount + ' = $' + (amount / price).toFixed(6)
         }]);
     }
 }
 
-function gotoYahoo(text, disposition){
+function gotoYahoo(text, disposition) {
     window.open('http://finance.yahoo.com/q?s=USDCNY=X');
 }
 
 var url = 'http://query.yahooapis.com/v1/public/yql?q=select%20Rate%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22USDCNY%22)&env=store://datatables.org/alltableswithkeys&format=json';
 var price;
 
-httpRequest(url, function(r){
+httpRequest(url, function (r) {
     price = JSON.parse(r);
     price = price.query.results.rate.Rate;
     price = Number(price);
 });
 
-chrome.omnibox.setDefaultSuggestion({'description':'Find current USD price.'});
+chrome.omnibox.setDefaultSuggestion({ 'description': '微医自己的汇率查询工具呀～' });
 
 chrome.omnibox.onInputChanged.addListener(updateAmount);
 
